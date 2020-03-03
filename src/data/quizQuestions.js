eada8d81658787f69b1a8e3b6249ed41ca400193
andreapell <andreadepell@gmail.com>
2020-03-02 16:17:24 -0800

    add more questions

--- src/data/quizQuestions.js
+++ src/data/quizQuestions.js
@@ -1,110 +1,182 @@
 var quizQuestions = [
   {
-      question: "Given the choice, would you rather invent a potion that would guarantee you:",
-      answers: [
-          {
-              type: "Gryffindor",
-              content: "Glory"
-          },
-          {
-              type: "Ravenclaw",
-              content: "Wisdom"
-          },
-          {
-              type: "Hufflepuff",
-              content: "Love"
-          },
-          {
-              type: "Slytherin",
-              content: "Power"
-          }
-      ]
-  },
-  {
-      question: "Which road tempts you most?",
-      answers: [
-          {
-              type: "Gryffindor",
-              content: "The narrow, dark, lantern-lit alley"
-          },
-          {
-              type: "Slytherin",
-              content: "The twisting, leaf-strewn path through woods"
-          },
-          {
-            type: "Ravenclaw",
-            content: "The cobbled street lined with ancient buildings"
-          },
-          {
-            type: "Hufflepuff",
-            content: "The wide, sunny, grassy lane"
+    question: "Given the choice, would you rather invent a potion that would guarantee you:",
+    answers: [
+        {
+            type: "Gryffindor",
+            content: "Glory"
         },
-      ]
-  },
-  {
-      question: "You enter an enchanted garden. What would you be most curious to examine first?",
-      answers: [
-          {
-              type: "Slytherin",
-              content: "The silver leafed tree bearing golden apples"
-          },
-          {
-              type: "Hufflepuff",
-              content: "The fat red toadstools that appear to be talking to each other"
-          },
-          {
-              type: "Gryffindor",
-              content: "The bubbling pool, in the depths of which something luminous is swirling"
-          },
-          {
+        {
             type: "Ravenclaw",
-            content: "The statue of an old wizard with a strangely twinkling eye"
-          }
-      ]
+            content: "Wisdom"
+        },
+        {
+            type: "Hufflepuff",
+            content: "Love"
+        },
+        {
+            type: "Slytherin",
+            content: "Power"
+        }
+    ]
   },
   {
-      question: "Once every century, the Flutterby bush produces flowers that adapt their scent to attract the unwary. If it lured you, it would smell of:",
-      answers: [
-          {
-            type: "Hufflepuff",
-            content: "Home"
-          },
-          {
-              type: "Gryffindor",
-              content: "A crackling log fire"
-          },
-          {
-              type: "Ravenclaw",
-              content: "Fresh parchment"
-          },
-          
-          {
+    question: "Which road tempts you most?",
+    answers: [
+      {
+          type: "Slytherin",
+          content: "The twisting, leaf-strewn path through woods"
+      },
+      {
+        type: "Hufflepuff",
+        content: "The wide, sunny, grassy lane"
+      },
+      {
+        type: "Ravenclaw",
+        content: "The cobbled street lined with ancient buildings"
+      },
+      {
+        type: "Gryffindor",
+        content: "The narrow, dark, lantern-lit alley"
+      },
+    ]
+  },
+  {
+    question: "You enter an enchanted garden. What would you be most curious to examine first?",
+    answers: [
+        {
+          type: "Hufflepuff",
+          content: "The fat red toadstools that appear to be talking to each other"
+        },
+        {
             type: "Slytherin",
-            content: "The Sea"
+            content: "The silver leafed tree bearing golden apples"
+        },
+        {
+            type: "Gryffindor",
+            content: "The bubbling pool, in the depths of which something luminous is swirling"
+        },
+        {
+          type: "Ravenclaw",
+          content: "The statue of an old wizard with a strangely twinkling eye"
+        }
+    ]
+  },
+  {
+    question: "Once every century, the Flutterby bush produces flowers that adapt their scent to attract the unwary. If it lured you, it would smell of:",
+    answers: [
+        {
+          type: "Ravenclaw",
+          content: "Fresh parchment"
+        },
+        {
+          type: "Gryffindor",
+          content: "A crackling log fire"
+        },
+        {
+          type: "Hufflepuff",
+          content: "Home"
+        },
+        {
+          type: "Slytherin",
+          content: "The Sea"
+      }
+    ]
+  },
+  {
+    question: "You and two friends need to cross a bridge guarded by a river troll who insists on fighting one of you before he will let all of you pass. Do you:",
+    answers: [
+        {
+          type: "Slytherin",
+          content: "Suggest that all three of you should fight (without telling the troll."
+        },
+        {
+          type: "Ravenclaw",
+          content: "Suggest drawing lots to decide which of you will fight."
+        },
+        {
+            type: "Hufflepuff",
+            content: "Attempt to confuse the troll into letting all three of you pass without fighting."
+        },
+        {
+          type: "Gryffindor",
+          content: "Volunteer to fight."
+        }
+    ]
+  }, 
+  {
+    question: "After you have died, what would you most like people to do when they hear your name?",
+    answers: [
+        {
+          type: "Hufflepuff",
+          content: "Miss you, but smile"
+        },
+        {
+          type: "Ravenclaw",
+          content: "Ask for more stories about your adventures."
+        },
+        {
+            type: "Gryffindor",
+            content: "Think with admiration of you achievements."
+        },
+        {
+          type: "Slytherin",
+          content: "I don't care what people think of me after I'm dead; it's what they think of me while I'm alive that counts."
         }
       ]
-  },
-  {
-      question: "You and two friends need to cross a bridge guarded by a river troll who insists on fighting one of you before he will let all of you pass. Do you:",
+    }, 
+    {
+      question: "Dawn or Dusk?",
       answers: [
-          {
-              type: "Hufflepuff",
-              content: "Attempt to confuse the troll into letting all three of you pass without fighting."
-          },
-          {
-              type: "Ravenclaw",
-              content: "Suggest drawing lots to decide which of you will fight."
-          },
-          {
-              type: "Slytherin",
-              content: "Suggest that all three of you should fight (without telling the troll."
-          },
-          {
-            type: "Gryffindor",
-            content: "Volunteer to fight."
-          }
+        {
+          type: "Gryffindor",
+          content: "Dawn"
+        },
+        {
+          type: "Ravenclaw",
+          content: "Dusk"
+        },
       ]
-  }
+    }, 
+    {
+      question: "Black or White?",
+      answers: [
+        {
+            type: "Slytherin",
+            content: "Black"
+        },
+        {
+          type: "Hufflepuff",
+          content: "White"
+        }
+      ]
+    }, 
+    {
+      question: "Forest or River?",
+      answers: [
+        {
+            type: "Slytherin",
+            content: "Forest"
+        },
+        {
+          type: "Ravenclaw",
+          content: "River"
+        }
+      ]
+    }, 
+    {
+      question: "Moon or Stars?",
+      answers: [
+        {
+            type: "Hufflepuff",
+            content: "Moon"
+        },
+        {
+          type: "Gryffindor",
+          content: "Stars"
+        }
+      ]
+    }, 
 ];
 
 export default quizQuestions;
