74dfd721375bf6ee4794fd3cad0fb15439c47d79
andreapell <andreadepell@gmail.com>
2020-03-03 09:22:34 -0800

    Updates

---
+++ precache-manifest.88234c6b909196e7410fce46ece3fedc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f3aecf87728cd771e3c29df54fb892",
+    "url": "/sorting-hat/index.html"
+  },
+  {
+    "revision": "160ed7b7bb21a34b095d",
+    "url": "/sorting-hat/static/css/main.758093ac.chunk.css"
+  },
+  {
+    "revision": "ba5d6895ee2d9402b0b9",
+    "url": "/sorting-hat/static/js/2.1a17ba02.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/sorting-hat/static/js/2.1a17ba02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160ed7b7bb21a34b095d",
+    "url": "/sorting-hat/static/js/main.aed0ec0a.chunk.js"
+  },
+  {
+    "revision": "2deda8b867d9c317da70",
+    "url": "/sorting-hat/static/js/runtime-main.ff214138.js"
+  },
+  {
+    "revision": "949bcc9d07186366acede8d82db5522f",
+    "url": "/sorting-hat/static/media/gryffindor.949bcc9d.png"
+  },
+  {
+    "revision": "093955fba76bfaf08ba037ded8772e61",
+    "url": "/sorting-hat/static/media/hufflepuff.093955fb.png"
+  },
+  {
+    "revision": "3bc430892b05aa1fab162aa6db1c120b",
+    "url": "/sorting-hat/static/media/ravenclaw.3bc43089.png"
+  },
+  {
+    "revision": "d3d18c2c26eee0a4d8e85ea9504be5c0",
+    "url": "/sorting-hat/static/media/slytherin.d3d18c2c.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sorting-hat/precache-manifest.88234c6b909196e7410fce46ece3fedc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sorting-hat/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
